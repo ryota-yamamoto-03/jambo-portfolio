1d77118247fa9d04543914126f9142abf29ca7c1
ryota-yamamoto-03 <ryota.yamamoto03@gmail.com>
2025-06-27 14:17:02 +0900

    ツイート機能を改修しました

--- js/main.js
+++ js/main.js
@@ -11,3 +11,22 @@ const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     document.getElementById('mobile-warning').style.display = 'block';
     document.getElementById('main-content').style.display = 'none';
   }
+
+// 右クリックを無効化
+  document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  });
+
+// ツイート機能
+document.addEventListener('mouseup', function () {
+    const selectedText = window.getSelection().toString().trim();
+
+    if (selectedText.length > 0) {
+      const siteURL = "https://ryota-yamamoto-03.github.io/jambo-portfolio/"; 
+      const tweetText = encodeURIComponent(`${selectedText}\n${siteURL}`);
+      const tweetURL = `https://twitter.com/intent/tweet?text=${tweetText}`;
+
+      // 新しいタブでツイート画面を開く
+      window.open(tweetURL, '_blank');
+    }
+  });
