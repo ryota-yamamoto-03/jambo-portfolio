640faa0fce8a6ab1abc5e094a5ba62ad2480de71
ryota-yamamoto-03 <ryota.yamamoto03@gmail.com>
2025-06-27 13:44:40 +0900

    PC端末オンリーにしました

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ const hamburger = document.getElementById('hamburger');
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
   });
+
+// PC専用サイトです
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  if (isMobile) {
+    document.getElementById('mobile-warning').style.display = 'block';
+    document.getElementById('main-content').style.display = 'none';
+  }
